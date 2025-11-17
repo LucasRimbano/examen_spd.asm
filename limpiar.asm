45bdca513add6d6f961321aa13302c6875044ae4
LucasRimbano <158349429+LucasRimbano@users.noreply.github.com>
2025-11-17 16:04:30 -0300

    Add limpiar_buffer_teclado to clear keyboard buffer
    Added limpiar_buffer_teclado procedure to clear keyboard buffer.

--- limpiar.asm
+++ limpiar.asm
@@ -2,38 +2,37 @@
 .model small
 
 .data
-; (sin dependencias con DS, es todo BIOS)
-dummy db 0
+; (  todo uso de la  BIOS)
+dummy  db 0
 dummy2 db 0
 
 .code
 public cls_attr_10h
 public cls_azul_10h
 public setmodo_80x25
+public limpiar_buffer_teclado    ; <<< NUEVO: exportamos la rutina
 
+;----------------------------------------------------------
 ; setmodo_80x25: modo 03h (80x25, 16 colores)
-; Preserva nada (rápida). Úsala si querés estandarizar el modo.
-
+;----------------------------------------------------------
 setmodo_80x25 proc
     mov ax, 0003h      ; AH=00h set video mode, AL=03h -> 80x25 color
     int 10h
     ret
 setmodo_80x25 endp
 
-
+;----------------------------------------------------------
 ; cls_attr_10h
 ; EN:  BL = atributo (BG<<4 | FG). Ej: 1Fh = fondo azul, texto blanco
 ; FX:  Limpia toda la pantalla con ese atributo y cursor a (0,0)
 ; RG:  Preserva AX,BX,CX,DX
-; Usa: INT 10h / AH=06h (scroll up/clear window con atributo)
-
+;----------------------------------------------------------
 cls_attr_10h proc
     push ax
     push bx
     push cx
     push dx
 
-    ; Limpiar toda la ventana con el atributo en BH
     mov ah, 06h        ; scroll up / clear window
     xor al, al         ; AL=0 -> limpiar toda la ventana
     mov bh, bl         ; BH = atributo pasado por el llamador
@@ -42,8 +41,7 @@ cls_attr_10h proc
     mov dl, 79         ; columna inferior (0..79)
     int 10h
 
-    ; Poner cursor en (0,0) página 0
-    mov ah, 02h
+    mov ah, 02h        ; cursor a (0,0)
     xor bh, bh
     xor dh, dh
     xor dl, dl
@@ -56,10 +54,9 @@ cls_attr_10h proc
     ret
 cls_attr_10h endp
 
-
+;----------------------------------------------------------
 ; cls_azul_10h: atajo fondo azul, texto blanco brillante (1Fh)
-; RG: Preserva AX,BX,CX,DX
-
+;----------------------------------------------------------
 cls_azul_10h proc
     push bx
     mov bl, 1Fh        ; BG=1 (azul) <<4 | FG=0Fh (blanco brillante)
@@ -67,5 +64,26 @@ cls_azul_10h proc
     pop bx
     ret
 cls_azul_10h endp
+
+;----------------------------------------------------------
+; limpiar_buffer_teclado
+; Vacía el buffer de teclado (descarta todas las teclas pendientes)
+;----------------------------------------------------------
+limpiar_buffer_teclado proc
+    push ax
+
+flush_loop:
+    mov ah, 01h        ; ¿hay tecla en el buffer?
+    int 16h
+    jz  lb_fin         ; ZF=1 → no hay más teclas
+
+    mov ah, 00h        ; leer y descartar la tecla
+    int 16h
+    jmp flush_loop
+
+lb_fin:
+    pop ax
+    ret
+limpiar_buffer_teclado endp
 
 end
